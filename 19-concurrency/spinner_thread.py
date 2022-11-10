e39bc45fa244558dfdd9ace4e283577e47094f20
Alfred Tso <32275603+alfredtso@users.noreply.github.com>
2022-11-10 23:06:29 +0800

    made some log with non-suspending br to understand more

--- 19-concurrency/spinner_thread.py
+++ 19-concurrency/spinner_thread.py
@@ -9,6 +9,31 @@ import itertools
 import time
 from threading import Thread, Event
 
+def dotdotdot(msg: str, done: Event) -> None:
+    to_print = [".", "..", "..."]
+    for c in itertools.cycle(to_print):
+        status = f'\r{c} {msg}'
+        print(status, end='', flush=True)
+        if done.wait(.1):  # <4>
+            break  # <5>
+    blanks = ' ' * len(status)
+    print(f'\r{blanks}\r', end='')
+
+def fast():
+    time.sleep(10)
+    return "diu nei"
+
+def sup2():
+    done = Event()
+    t1 = Thread(target=dotdotdot, args=("what!", done))
+    print(f'dotdotdot object: {t1}')
+    t1.start()
+    time.sleep(0.1)
+    res = fast()
+    done.set()
+    t1.join()
+    return res
+
 def spin(msg: str, done: Event) -> None:  # <1>
     for char in itertools.cycle(r'\|/-'):  # <2>
         status = f'\r{char} {msg}'  # <3>
@@ -35,7 +60,7 @@ def supervisor() -> int:  # <1>
     return result
 
 def main() -> None:
-    result = supervisor()  # <9>
+    result = sup2()  # <9>
     print(f'Answer: {result}')
 
 if __name__ == '__main__':
